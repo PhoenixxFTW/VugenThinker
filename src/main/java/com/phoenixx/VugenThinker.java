91a56f81282389da47f456dfcdc1945e1c476ea7
Phoenix <talpurjunaid5@gmail.com>
2023-02-21 09:25:59 -0500

    Implementing user control logic

--- src/main/java/com/phoenixx/VugenThinker.java
+++ src/main/java/com/phoenixx/VugenThinker.java
@@ -6,6 +6,7 @@ import java.net.URLDecoder;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.Scanner;
 
 /**
  * @author Junaid Talpur
@@ -20,8 +21,10 @@ public class VugenThinker {
 
     public static int thinkTime = 0;
     public static boolean limitThinkTime = false;
+    public static boolean excludedScripts = false;
 
     public static List<VugenScript> loadedScripts = new ArrayList<>();
+    public static List<VugenScript> selectedScripts = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         System.out.println();
@@ -44,6 +47,10 @@ public class VugenThinker {
         File folder = new File(currentPath);
         if(!folder.isDirectory()) {
             System.out.println("Current location is not a directory!");
+            return;
+        }
+        if(Objects.requireNonNull(folder.list()).length == 0) {
+            System.out.println("No scripts found in folder: " + folder.getName());
             return;
         }
 
@@ -69,28 +76,125 @@ public class VugenThinker {
             }
         }
 
-        System.out.println("================= Script details =================");
-        System.out.println("All scripts found: ");
-
-        int count = 1;
+        System.out.println("Loading vugen scripts...");
         // Script folders
         for(File scriptFolder: vugenScripts) {
             if(!scriptFolder.isDirectory()) {
                 System.out.println("Script file: " + scriptFolder.getName() + " IS NOT A DIRECTORY. Cancelling...");
                 continue;
             }
-
             VugenScript vugenScript = VugenScript.buildScript(scriptFolder);
-            System.out.println(count+") " + vugenScript.getScriptFile().getName());
+            loadedScripts.add(vugenScript);
+        }
+        System.out.println("Loaded " + loadedScripts.size() + " scripts!");
+
+        // TODO Main logic
+        /**
+         * Prompt the user to enter an option to either:
+         *      1) View all scripts
+         *      2) Settings
+         *          a) Set think-time
+         *          b) enable / disable think-time limit
+         */
+
+        int option = -1;
+        Scanner input = new Scanner(System.in);
+
+        while (option != 0) {
+            System.out.println("\n======================= Vugen Thinker Main Menu =======================");
+            System.out.println("Select an option:");
+            System.out.println("0) Exit");
+            System.out.println("1) View scripts in current folder");
+            System.out.println("2) Settings");
+            System.out.println("3) Apply changes");
+            System.out.print("> ");
+            option = input.nextInt();
+
+            switch (option) {
+                case 0:
+                    // Exit application
+                    System.out.println("Exiting...");
+                    break;
+                case 1:
+                    System.out.println();
+                    displayLoadedScripts();
+                    break;
+                case 2:
+                    int settingsOptions = -1;
+                    while (true) {
+                        System.out.println("\n====================== Settings Menu =====================");
+                        System.out.println("Select an option: ");
+                        System.out.println("0) Return to main menu");
+                        System.out.println("1) Set think time");
+                        System.out.println("2) Enable / Disable think time limiter");
+                        System.out.println("3) Include Scripts");
+                        System.out.println("4) Exclude Scripts");
+                        System.out.println("5) View selected settings");
+                        System.out.print("> ");
+                        settingsOptions = input.nextInt();
+
+                        if(settingsOptions == 0) {
+                            break;
+                        } else if(settingsOptions == 1) {
+                            System.out.print("\nInput think time (0-999): ");
+                            thinkTime = input.nextInt();
+                        } else if(settingsOptions == 2) {
+                            limitThinkTime = !limitThinkTime;
+                            System.out.println((limitThinkTime ? "ENABLED" : "DISABLED") + " think time limit!");
+                        } else if (settingsOptions == 3) {
+                            excludedScripts = false;
+                            System.out.println("\n============== Script Inclusion Menu ==============");
+                            scriptSelection(input);
+                        } else if(settingsOptions == 4) {
+                            excludedScripts = true;
+                            System.out.println("\n============== Script Exclusion Menu ==============");
+                            scriptSelection(input);
+                        } else if(settingsOptions == 5) {
+                            System.out.println("\n============== VugenThinker Selected Settings ==============");
+                            System.out.println("Think time: " + thinkTime);
+                            System.out.println("Think time limiter: " + (limitThinkTime ? "ENABLED" : "DISABLED"));
+                            System.out.println((excludedScripts ? "Excluded" : "Included") + " Scripts: " + selectedScripts.size());
+                            for(int i = 0; i < selectedScripts.size(); i++) {
+                                System.out.println("\t" +(i+1)+") " + selectedScripts.get(i).getScriptFile().getName());
+                            }
+                        } else {
+                            System.out.println("Invalid option!");
+                        }
+                    }
+
+                    break;
+                case 3:
+                    break;
+                default:
+                    System.out.println("Invalid option!");
+            }
+        }
+    }
+
+    public static void scriptSelection(Scanner input) {
+        displayLoadedScripts();
+
+        int option = -1;
+        while (true) {
+            System.out.print("Enter the ID of a script (0 to exit): ");
+            option = input.nextInt();
+
+            if(option > 0 && option < loadedScripts.size()) {
+                // Get the selected script via its index(-1) and add it to our new list
+                selectedScripts.add(loadedScripts.get(option-1));
+            } else {
+                break;
+            }
+        }
+    }
+
+    public static void displayLoadedScripts() {
+        System.out.println("All scripts: " + loadedScripts.size());
+        for(int i = 0; i < loadedScripts.size(); i++) {
+            VugenScript vugenScript = loadedScripts.get(i);
+            System.out.println((i+1)+") " + vugenScript.getScriptFile().getName());
             System.out.println("\t> Config File: " + vugenScript.getConfigName());
             System.out.println("\t> Action files: " + vugenScript.getActionFiles());
-
-            vugenScript.setThinkTime(15);
-            vugenScript.updateScript();
-
-            loadedScripts.add(vugenScript);
-
-            count++;
         }
     }
 }
